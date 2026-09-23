92b4969e498e594cdaadca5513bf83573d754101
Michael Jones <michael.jones90@me.com>
2026-09-23 13:40:34 +0100

    Initial Commit

---
+++ HackingWithSwift-SnowSeeker/HackingWithSwift_SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_SnowSeekerApp.swift
+//  HackingWithSwift-SnowSeeker
+//
+//  Created by Michael Jones on 23/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
